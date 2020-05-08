535f163f36ab997334f67c9c1158dc431238f81e
Leonardo Vizcaya <vizcayaleonardo@gmail.com>
2020-05-08 18:28:52 -0400

    Jwk schema and lib change.

--- src/controllers/jwk.comtroller.ts
+++ src/controllers/jwk.comtroller.ts
@@ -2,15 +2,13 @@ import { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 
-var rsaPemToJwk = require('rsa-pem-to-jwk');
+const pem2jwk = require('pem-jwk').pem2jwk
 
 const jwk = async (req: Request, res: Response): Promise<Response> => {
     const pathToPubKey = path.join(__dirname, '../keys', 'id_rsa_pub.pem');
     const pem = fs.readFileSync(pathToPubKey, 'utf8');
-    var jwkJson = rsaPemToJwk(pem, {use: 'sig', alg: 'RS256'}, 'public');
-    return res.status(200).send({
-        keys: [jwkJson]
-    });
+    var jwkJson = await pem2jwk(pem)
+    return res.status(200).send({keys: [jwkJson]});
 }
 
 export {jwk}
