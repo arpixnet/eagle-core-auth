988cde67ea8cd7fb2871cb4222936b5ef904adb6
Leonardo Vizcaya Savchenko <lvizcaya@arpixnet.com>
2022-05-15 11:17:36 -0500

    Borrados los requerimientos.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -42,16 +42,4 @@ router.delete('/user', passport.authenticate('jwt', { session: false }), deleteU
 router.get('/user', passport.authenticate('jwt', { session: false }), account);
 router.patch('/user', passport.authenticate('jwt', { session: false }), updateUser);
 
-// x - Actualizar usuario
-// Falta implementar los endpoints para agregar, modificar y eliminar el/los role(s) al usuario.
-// SMS y SMS verification.
-// Poder implementar plugins.
-// Implementar, poder configurar dificultad de la contraseña.
-// Templates de email, sms, etc que se extraigan de la base de datos.
-// Segundo factor de autenticación, google o sms.
-// x - Convertir los claims en claims dinámicos, que pueda agregarlo a la base de datos y aplicarlo al token por medio de lambda.
-// Los endpoints adminitrativos para cambiar password, email, roles, verificar email, visualizar, crear y eliminar usuarios.
-// Agregar una función que genere una contraseña aleatoria para la creación con endpoint administrativo.
-// Webhooks de las acciones signup, signin, change-email, delete.
-
 export default router;
